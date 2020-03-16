8e16c9724f0eed3c3fc8d47b752950616d7f76cc
kakke18 <tanaka-kakeru-xm@ynu.jp>
2020-03-16 23:35:26 +0900

    add: link

--- pages/links.vue
+++ pages/links.vue
@@ -4,11 +4,18 @@
     <v-flex class="text-center">
       <v-row justify="center">
         <v-col cols="6">
-          <v-img width="600" class="img-title" src="/images/title.png" />
+          <title-image />
           <div>
             このページでは<span class="font--primary">リンク</span
             >を貼っています．
           </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col v-for="item in items" :key="item.key">
+          <a class="link" :href="item.link" target="_blank">
+            <v-icon large>{{ item.icon }}</v-icon>
+          </a>
         </v-col>
       </v-row>
     </v-flex>
@@ -17,9 +24,42 @@
 
 <script>
 import Header from '~/components/Header.vue'
+import TitleImage from '~/components/TitleImage.vue'
 export default {
   components: {
-    Header
+    Header,
+    TitleImage
+  },
+  data() {
+    return {
+      items: [
+        {
+          key: 'github',
+          icon: 'mdi-github',
+          link: 'https://github.com/bonochof/'
+        },
+        {
+          key: 'twitter',
+          icon: 'mdi-twitter',
+          link: 'https://twitter.com/bonochof/'
+        },
+        {
+          key: 'scrapbox',
+          icon: 'mdi-alpha-s-circle',
+          link: 'https://scrapbox.io/ashiato-note/'
+        },
+        {
+          key: 'youtube',
+          icon: 'mdi-youtube',
+          link: 'https://www.youtube.com/channel/UCF8ViNqQffiG0WvSjP-bfNQ'
+        },
+        {
+          key: 'cloud',
+          icon: 'mdi-cloud-download',
+          link: 'http://ux.getuploader.com/ashiato01/'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -27,5 +67,8 @@ export default {
 <style scoped>
 .font--primary {
   color: #d2691e;
+}
+.link {
+  text-decoration: none;
 }
 </style>
