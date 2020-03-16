67578b030f3ecc58774ef2434f537e6293260e7b
kakke18 <tanaka-kakeru-xm@ynu.jp>
2020-03-17 00:55:54 +0900

    fix: title image

--- pages/research.vue
+++ pages/research.vue
@@ -4,7 +4,7 @@
     <v-flex class="text-center">
       <v-row justify="center">
         <v-col cols="6">
-          <v-img width="600" class="img-title" src="/images/title.png" />
+          <title-image />
           <div>
             このページでは私の<span class="font--primary">研究</span
             >を紹介します．
@@ -17,9 +17,11 @@
 
 <script>
 import Header from '~/components/Header.vue'
+import TitleImage from '~/components/TitleImage.vue'
 export default {
   components: {
-    Header
+    Header,
+    TitleImage
   }
 }
 </script>
