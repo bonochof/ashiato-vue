b4199ce9a64a54321d238deecbd5dee9a83daddc
kakke18 <tanaka-kakeru-xm@ynu.jp>
2020-03-17 00:54:01 +0900

    add: game

--- pages/games.vue
+++ pages/games.vue
@@ -4,22 +4,81 @@
     <v-flex class="text-center">
       <v-row justify="center">
         <v-col cols="6">
-          <v-img width="600" class="img-title" src="/images/title.png" />
+          <title-image />
           <div>
             このページでは<span class="font--primary">自作ゲーム</span
             >を紹介します．
           </div>
         </v-col>
       </v-row>
+      <v-card>
+        <v-row>
+          <v-col v-for="item in items" :key="item.key" cols="4">
+            <v-card-title>{{ item.title }}</v-card-title>
+            <a :href="item.link" target="_blank">
+              <v-img :src="item.src" class="img-game" />
+            </a>
+            <v-card-text class="text-left">{{ item.text }}</v-card-text>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
 import Header from '~/components/Header.vue'
+import TitleImage from '~/components/TitleImage.vue'
 export default {
   components: {
-    Header
+    Header,
+    TitleImage
+  },
+  data() {
+    return {
+      items: [
+        {
+          key: 'othello',
+          title: 'Minimax探索オセロ',
+          link: 'https://ashiato.kenkenpa.net/games/minimax-othello/index.html',
+          src: '/images/games/othello.PNG',
+          text:
+            '高専4年(19歳)のときに授業で作成したオセロをDXOpalに移植した．Minimax探索するコンピュータを実装している．'
+        },
+        {
+          key: 'apple',
+          title: 'りんご拾い',
+          link: 'https://ashiato.kenkenpa.net/games/apple-catch/index.html',
+          src: '/images/games/apple.PNG',
+          text: 'DXOpalを構ってみた．るびまを参考にして作ったゲーム．'
+        },
+        {
+          key: 'messtory',
+          title: '♀tory (メストーリー)',
+          link: 'http://ux.getuploader.com/ashiato01/download/2/messtory1.zip',
+          src: '/images/games/messtory.PNG',
+          text:
+            '高専1年(16歳)のときに作成した初めてのゲーム．アクションゲームを作りたかったが当時はCUIものしか作れなかったため人型によく似た「♀」を主人公に見立てた．ソースコードがひどい．'
+        },
+        {
+          key: 'messtory2',
+          title: '♀tory改 (メストーリー改)',
+          link: 'http://ux.getuploader.com/ashiato01/download/3/messtory2.zip',
+          src: '/images/games/messtory2.PNG',
+          text:
+            '高専2年(17歳)のときに作成したゲーム．GUIを学び，♀toryの続編を作った．DXライブラリを使用した．'
+        },
+        {
+          key: 'kbdefence',
+          title: 'KeaBoarDefence (キーボーディフェンス)',
+          link:
+            'http://ux.getuploader.com/ashiato01/download/4/KeaBoarDefence.zip',
+          src: '/images/games/kbdefence.PNG',
+          text:
+            '高専3年(18歳)のときに作成したゲーム．ディフェンスゲーム(自分の城を守りつつ相手の城を攻めるタイプのゲーム)を作りたいと思い，キーボードの英字キーを兵士に見立てたディフェンスゲームを作成した．DXライブラリを使用した．'
+        }
+      ]
+    }
   }
 }
 </script>
@@ -27,5 +86,8 @@ export default {
 <style scoped>
 .font--primary {
   color: #d2691e;
+}
+.img-game {
+  border: black solid;
 }
 </style>
