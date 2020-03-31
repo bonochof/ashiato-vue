4fea40693d3f5297ee403d9de573c4c0f6071406
bonochof <ryos427@yahoo.co.jp>
2020-04-01 03:23:35 +0900

    add products page

---
+++ pages/products.vue
@@ -0,0 +1,98 @@
+<template>
+  <v-container>
+    <Header title="あしあと - Products" />
+    <v-row justify="center">
+      <v-col md="6" cols="12">
+        <title-image />
+        <div class="text-center intro-text">
+          このページでは試作品を含めた<span class="font--primary"
+            >プロダクト</span
+          >を紹介します．
+        </div>
+      </v-col>
+    </v-row>
+    <v-card>
+      <v-row>
+        <v-col
+          v-for="item in items"
+          :key="item.key"
+          lg="4"
+          md="4"
+          sm="6"
+          cols="12"
+        >
+          <v-card-title>{{ item.title }}</v-card-title>
+          <a :href="item.link" target="_blank">
+            <v-img :src="item.src" class="img-game" />
+          </a>
+          <v-card-text class="text-left">{{ item.text }}</v-card-text>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import TitleImage from '~/components/TitleImage.vue'
+export default {
+  components: {
+    Header,
+    TitleImage
+  },
+  data() {
+    return {
+      items: [
+        {
+          key: 'tea',
+          title: 'お茶シミュレータ',
+          link:
+            'https://ashiato.kenkenpa.net/products/tea-simulation/index.html',
+          src: '/images/products/tea.png',
+          text: 'お茶が飲みやすい温度になるまでの時間を求めるシミュレータ．'
+        },
+        {
+          key: 'p2p-chat',
+          title: 'P2P Chat',
+          link: 'https://ashiato.kenkenpa.net/products/p2p-chat/index.html',
+          src: '/images/products/p2p-chat.png',
+          text: 'Peer to Peerで通信を行うチャットシステム．'
+        },
+        {
+          key: 'zundoko',
+          title: 'ズンドコキヨシプログラム',
+          link: 'https://ashiato.kenkenpa.net/products/zundoko/index.html',
+          src: '/images/products/zundoko.png',
+          text:
+            '「ズン」と「ドコ」をランダム生成し，「ズン ズン ズン ズン ドコ」と並んだら「キヨシ！」を生成するプログラム．音声付き．'
+        },
+        {
+          key: 'ginrummy',
+          title: 'GinRummy InfluenceMap',
+          link:
+            'https://ashiato.kenkenpa.net/products/ginrummy-influencemap/index.html',
+          src: '/images/products/ginrummy.png',
+          text: 'トランプゲーム「ジンラミー」の影響マップ．'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.font--primary {
+  color: #d2691e;
+}
+.img-game {
+  border: black solid;
+}
+.intro-text {
+  font-size: 10px;
+}
+@media only screen and (min-width: 651px) {
+  .intro-text {
+    font-size: 16px;
+  }
+}
+</style>
